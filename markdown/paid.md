e0254f8bfcbe5cb02e0c68f8de51bce55ac894ef
michael <michael.ampm@gmail.com>
2021-10-24 22:44:31 -0700

    delete old

--- markdown/paid.md
+++
@@ -1,3 +0,0 @@
-# Paid Modules
-
-This category contain all paid modules from Kokopelli. When paid module reaches some amount of sales it's being open-sourced and published under GPLv3.
--- node_modules/nuxt-i18n-module/src/templates/components/i18n-switcher.vue
+++
@@ -1,7 +0,0 @@
-<template>
-  <ul class="language-switcher">
-    <li v-for="(lang) in Object.keys($i18n.messages)" :key="lang">
-      <nuxt-link class="language-switcher-link" :to="{ params: {lang: i18nLangParam(lang)} }" exact>{{ lang }}</nuxt-link>
-    </li>
-  </ul>
-</template>
