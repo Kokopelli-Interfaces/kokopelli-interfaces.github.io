af7384d14a406d337316b8fc13e0072826503d03
michael <michael.ampm@gmail.com>
2021-10-27 19:19:46 -0700

    Updated deployment.

---
+++ .nuxt/components/i18n-switcher.vue
@@ -0,0 +1,7 @@
+<template>
+  <ul class="language-switcher">
+    <li v-for="(lang) in Object.keys($i18n.messages)" :key="lang">
+      <nuxt-link class="language-switcher-link" :to="{ params: {lang: i18nLangParam(lang)} }" exact>{{ lang }}</nuxt-link>
+    </li>
+  </ul>
+</template>
---
+++ .nuxt/router.js
@@ -0,0 +1,44 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import { interopDefault } from './utils'
+
+Vue.use(Router)
+
+const scrollBehavior = function (to, from, savedPosition) {
+      // if the returned position is falsy or an empty object,
+      // will retain current scroll position.
+      let position = false
+
+      // if no children detected
+      if (to.matched.length < 2) {
+        // scroll to the top of the page
+        position = { x: 0, y: 0 }
+      } else if (to.matched.some((r) => r.components.default.options.scrollToTop)) {
+        // if one of the children has scrollToTop option set to true
+        position = { x: 0, y: 0 }
+      }
+
+      // savedPosition is only available for popstate navigations (back button)
+      if (savedPosition) {
+        position = savedPosition
+      }
+      if (to.hash && document.querySelector(to.hash)) {
+        // scroll to anchor by returning the selector
+        position = { selector: to.hash }
+      }
+      return position
+    }
+
+export function createRouter() {
+  return new Router({
+    mode: 'history',
+    base: decodeURI('/'),
+    linkActiveClass: 'nuxt-link-active',
+    linkExactActiveClass: 'nuxt-link-exact-active',
+    scrollBehavior,
+
+    routes: [],
+
+    fallback: false
+  })
+}
